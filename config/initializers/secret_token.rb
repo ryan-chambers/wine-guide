a64974ea68a55987ef2f905db761411c15889dcd
Ryan Chambers <ryan.b.chambers@gmail.com>
2012-08-19 17:13:56 -0400

    initial creation

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WineGuide::Application.config.secret_token = '34ea800dbdaf83daed8eaa5ba3279d6cb47e98c1c57399202774a2750d76dc20e4aec7e9e78570dd57303ee38cedb4920ccd26921b27d3122abef64bb41cc8c3'
