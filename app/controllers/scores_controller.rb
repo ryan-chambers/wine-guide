9f336dd26d9bc4c3eb7065e11e712e96d64976c3
Ryan Chambers <ryan.b.chambers@gmail.com>
2013-01-01 18:41:03 -0500

    add methods

--- app/controllers/scores_controller.rb
+++ app/controllers/scores_controller.rb
@@ -8,6 +8,15 @@ class ScoresController < ApplicationController
     end
   end
 
+  def show
+    @score = Score.find(params[:id])
+
+    respond_to do |format|
+      format.html
+      format.json { render :json => @wine}
+    end
+  end
+
   def create
     @wine = Wine.find(params[:wine_id])
     @score = @wine.scores.create(params[:score]) do |score|
@@ -17,5 +26,25 @@ class ScoresController < ApplicationController
     end
 
     redirect_to wine_path(@wine)
+  end
+
+  def edit
+    @score = Score.find(params[:id])
+  end
+
+  def update
+    @score = Score.find(params[:id])
+
+    respond_to do |format|
+      if @score.update_attributes(params[:score])
+        format.html  { redirect_to(@score,
+                      :notice => 'Score was successfully updated.') }
+        format.json  { head :no_content }
+      else
+        format.html  { render :action => "edit" }
+        format.json  { render :json => @score.errors,
+                      :status => :unprocessable_entity }
+      end
+    end
   end
 end
