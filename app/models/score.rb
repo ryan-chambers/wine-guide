90aa34c2059f6095276d46fb09e5b39c256b5783
Ryan Chambers <ryan.b.chambers@gmail.com>
2013-01-01 19:04:46 -0500

    fix bug

--- app/models/score.rb
+++ app/models/score.rb
@@ -6,8 +6,7 @@ class Score < ActiveRecord::Base
   belongs_to :wine
 
   def score_between_0_and_100
-    errors.add(:score, "The score must be between 0 and 100") if
-      score > 100 || score < 0
+    errors.add(:score, "The score must be between 0 and 100") if score.nil? || score > 100 || score < 0
   end
 
   def wine_price
