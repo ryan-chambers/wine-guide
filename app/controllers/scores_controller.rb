1eff6c47f45860707fd44079167f19f96e3c8939
Ryan Chambers <ryan.b.chambers@gmail.com>
2013-01-01 19:08:05 -0500

    restore removed

--- app/controllers/scores_controller.rb
+++ app/controllers/scores_controller.rb
@@ -19,8 +19,11 @@ class ScoresController < ApplicationController
 
   def create
     @wine = Wine.find(params[:wine_id])
-    @score = @wine.scores.create(params[:score])
-
+   @score = @wine.scores.create(params[:score]) do |score|
+      if score.in_fridge
+        score.score = 0
+      end
+    end
     redirect_to wine_path(@wine)
   end
 
