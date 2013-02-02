1e3199d2e254986a8c7791e012d2f68748df0a8e
Ryan Chambers <ryan.b.chambers@gmail.com>
2013-02-02 13:34:48 -0500

    don't store secret token in github

--- README.md
+++ README.md
@@ -2,3 +2,5 @@ wine-guide
 ==========
 
 Store wine ratings
+
+Set an environment variable WINE_GUIDE_TOKEN to contain the secret token.
--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-WineGuide::Application.config.secret_token = '34ea800dbdaf83daed8eaa5ba3279d6cb47e98c1c57399202774a2750d76dc20e4aec7e9e78570dd57303ee38cedb4920ccd26921b27d3122abef64bb41cc8c3'
+WineGuide::Application.config.secret_token = ENV['WINE_GUIDE_TOKEN']
