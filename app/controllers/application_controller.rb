bce65a05e7e2ce77556546fc0bcc61d91947c7f8
Ryan Chambers <ryan.b.chambers@gmail.com>
2013-02-07 19:11:14 -0500

    converted from basic to digest authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,12 @@
 class ApplicationController < ActionController::Base
+  USERS = {ENV['WINE_GUIDE_USERNAME'] => ENV['WINE_GUIDE_PASSWORD']}
+  
   protect_from_forgery
 
   protected
     def authenticate
-      authenticate_or_request_with_http_basic do |username, password|
-        username == ENV['WINE_GUIDE_USERNAME'] && password == ENV['WINE_GUIDE_PASSWORD']
+      authenticate_or_request_with_http_digest do |username|
+        USERS[username]
       end
     end
 end
