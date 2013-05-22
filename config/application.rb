3ec8d67fa984da6a68301108975c9842a20ba922
Ryan Chambers <ryan.b.chambers@gmail.com>
2013-05-22 07:05:55 -0400

    set time zone

--- config/application.rb
+++ config/application.rb
@@ -60,6 +60,9 @@ module WineGuide
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
 
+    config.time_zone = 'Eastern Time (US & Canada)'
+    config.active_record.default_timezone = 'Eastern Time (US & Canada)'
+ 
     config.generators do |g|
       g.fixture_replacement :factory_girl
     end
