60c58941316eb17bbf67ccef7c32a5e4cb293573
Ryan Chambers <ryan.b.chambers@gmail.com>
2013-12-15 18:24:30 -0500

    added Abruzzo

--- app/models/country.rb
+++ app/models/country.rb
@@ -37,7 +37,7 @@ class Country
   GERMANY = Country.new('Germany')
   GREECE = Country.new('Greece')
   HUNGARY = Country.new('Hungary')
-  ITALY = Country.new('Italy', ['Barolo', 'Barbaresco', 'Brunello di Montalcino', 'Rosso di Montalcino', 'Calabria', 'Docg Tuscany', 'Tuscany', 'Piedmont',
+  ITALY = Country.new('Italy', ['Abruzzo', 'Barolo', 'Barbaresco', 'Brunello di Montalcino', 'Rosso di Montalcino', 'Calabria', 'Docg Tuscany', 'Tuscany', 'Piedmont',
       'Doc Sicily', 'Sicily', 'Veneto', 'Igt Veneto', 'Igt Delle Venezie', 'Doc Vernaiolo', 'Doc Valdadige',
       'Igt Sicilia', 'Igt Toscana', 'Doc Piedmont', 'Vino Nobile Di Montepulciano', 'Doc Chianti', 'Doc Gioia del Colle',
       'Doc Abruzzo', 'Doc Veneto', 'Doc Puglia', 'Igt Campania', 'Igt Veronese', 'Valpolicella'])
