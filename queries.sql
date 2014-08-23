ef058e23171376ab1554f42a847f234f49569130
Ryan Chambers <ryan.b.chambers@gmail.com>
2014-08-23 15:37:15 -0400

    add British Columbia

--- queries.sql
+++ queries.sql
@@ -1,3 +1,7 @@
-select * from wines wines, wineries wineries
-where wines.winery_id = wineries.id
-and wineries.name = ''
+﻿--select * from wines wines, wineries wineries
+--where wines.winery_id = wineries.id
+--and wineries.name = ''
+
+--select * from wines where id = 283
+
+update wines set other = 'Antagonist', lcbo_code = '467662' where id = 283;
