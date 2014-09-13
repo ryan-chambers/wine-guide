6b68aaf503428475be59d4a6752b6073b45b674c
Ryan Chambers <ryan.b.chambers@gmail.com>
2014-09-13 13:19:07 -0400

    add Brazil

--- app/models/country.rb
+++ app/models/country.rb
@@ -18,6 +18,7 @@ class Country
   AUSTRALIA = Country.new('Australia', ['McLaren Vale', 'Western Australia', 'Margaret River', 'Barossa Valley', 'Clare Valley',
     'South Australia', 'Southeast Australia'])
   AUSTRIA = Country.new('Austria', ['Burgenland'])
+  BRAZIL = Country.new('Brazil')
   BULGARIA = Country.new('Bulgaria')
   CANADA = Country.new('Canada', ['VQA Beamsville Bench', 'VQA British Columbia', 'VQA Niagara', 'VQA Niagara Peninsula', 'VQA Niagara On The Lake', 
       'VQA Okanagan Valley', 'VQA Niagara River', 'VQA Creek Shores', 'VQA Lake Erie North Shore', 'VQA Lincoln Lakeshore'])
