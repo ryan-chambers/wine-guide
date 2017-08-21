98b46ce52bab8b2aa532a07df23e082660daec34
Ryan Chambers <ryan.b.chambers@gmail.com>
2017-08-21 15:17:24 -0400

    Implement region partial match

--- app/controllers/countries_controller.rb
+++ app/controllers/countries_controller.rb
@@ -2,13 +2,19 @@ class CountriesController < ApplicationController
   def index
     @country = params[:country]
     @region_partial = params[:region_partial]
-    p "Got country #{@country}"
-    p "Got region #{@region_partial}"
+    @regions = []
+#    p "Got country #{@country}"
+#    p "Got region #{@region_partial}"
 
     if Country.is_country?(@country)
-      logger.info "Found country #{@country}"
+#      logger.info "Found country #{@country}"
+      if @region_partial
+        @regions = Country::COUNTRIES[@country].regions.select { |r| r.downcase.include? @region_partial.downcase}
+      else
+        @regions = Country::COUNTRIES[@country].regions
+      end
       respond_to do |format|
-        format.json { render :json => Country::COUNTRIES[@country].regions}
+        format.json { render :json => @regions}
       end
     else
       logger.info "Invalid country #{@country}"
--- spec/controllers/countries_controller_spec.rb
+++ spec/controllers/countries_controller_spec.rb
@@ -5,10 +5,32 @@ describe CountriesController do
   describe "GET 'countries'" do
     it "returns partial search results for Canada - Niagara" do
       get 'index', params: {country: 'Canada', region_partial: 'Niagara'}, :format => :json
-      p "#{response.parsed_body}"
+#      p "#{response.parsed_body}"
       expect(response).to be_success
+      # TODO could parse to array and check all results
       expect(response.parsed_body).to include('VQA Niagara River')
       expect(response.parsed_body).not_to include('VQA Creek Shores')
+    end
+
+    it "returns partial search results for Canada - Creek" do
+      get 'index', params: {country: 'Canada', region_partial: 'Creek'}, :format => :json
+#      p "#{response.parsed_body}"
+      expect(response).to be_success
+      # TODO could parse to array and check all results
+      expect(response.parsed_body).not_to include('VQA Niagara River')
+      expect(response.parsed_body).to include('VQA Creek Shores')
+    end
+
+    it "returns full region list when partial region not provided" do
+      get 'index', params: {country: 'Canada'}, :format => :json
+#      p "#{response.parsed_body}"
+      expect(response).to be_success
+      expect(response.parsed_body).to include('VQA Niagara River')
+      expect(response.parsed_body).to include('VQA Creek Shores')
+    end
+
+    it "returns error for invalid country" do
+      # TODO
     end
   end
 end
