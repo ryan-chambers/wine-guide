75478c3da08d95e95c51cbaa39aa64265ea371fc
Ryan Chambers <ryan.b.chambers@gmail.com>
2017-08-21 15:01:39 -0400

    Add assertions for matching Canada, Niagara

--- spec/controllers/countries_controller_spec.rb
+++ spec/controllers/countries_controller_spec.rb
@@ -3,9 +3,12 @@ require 'rails_helper'
 describe CountriesController do
 
   describe "GET 'countries'" do
-    it "returns partial search results for Canada" do
-      get 'index', params: {country: 'Canada'}, :format => :json
+    it "returns partial search results for Canada - Niagara" do
+      get 'index', params: {country: 'Canada', region_partial: 'Niagara'}, :format => :json
+      p "#{response.parsed_body}"
       expect(response).to be_success
+      expect(response.parsed_body).to include('VQA Niagara River')
+      expect(response.parsed_body).not_to include('VQA Creek Shores')
     end
   end
 end
