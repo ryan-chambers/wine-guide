27d5c592edf8bbe6c8156e3714cb99b44fa519c0
Ryan Chambers <ryan.b.chambers@gmail.com>
2018-12-27 20:02:11 -0500

    Update

--- db/cellar.sql
+++ db/cellar.sql
@@ -1,3 +1,3 @@
-﻿select wy.name, w.year, w.other 
+﻿select wy.name, w.other, w.year, b.drink_to, b.price 
 from wines w, bottles b, wineries wy 
 where w.id = b.wine_id and b.in_fridge = 't' and w.winery_id = wy.id
