f6eeb50247a7d5b046956d3954f73a343d53760d
Ryan Chambers <ryan.b.chambers@gmail.com>
2018-12-27 20:41:17 -0500

    Show grapes

--- db/cellar.sql
+++ db/cellar.sql
@@ -1,3 +1,3 @@
-﻿select wy.name, w.other, w.year, b.drink_to, b.price 
-from wines w, bottles b, wineries wy 
-where w.id = b.wine_id and b.in_fridge = 't' and w.winery_id = wy.id
+﻿select wy.name, w.other, w.year, b.drink_to, b.price, g.name
+from wines w, bottles b, wineries wy, grapes_wines wg, grapes g
+where w.id = b.wine_id and b.in_fridge = 't' and w.winery_id = wy.id and g.id = wg.grape_id and wg.wine_id = w.id
