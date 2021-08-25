2e32c4a62053e033f152c48c189b945723d3fdf2
Ryan Chambers <ryan.b.chambers@gmail.com>
2021-08-25 19:19:33 -0400

    Fill out spec

--- spec/lib/wine_score_parser_spec.rb
+++ spec/lib/wine_score_parser_spec.rb
@@ -13,8 +13,13 @@ describe 'Wine Score Parser' do
       wb = parse_wine_bottle_line('Creekside, Syrah, 2015, Broken Press, VQA St Davids Bench. $55.0. 92/100. Outstanding leg of lamb wine.. Bought Dec 2017. From 2018. To 2026. [21 Jun 2020].', 'Canada')
 #      p "#{wb.to_s}"
       bottle1 = wb[:bottles][0];
+#      expect(bottle1.comments).to eq('Outstanding leg of lamb wine')
+      expect(bottle1.date).to eq('21 Jun 2020')
+      expect(bottle1.price).to eq('55')
       expect(bottle1.score).to eq(92)
-      expect(bottle1.data).to eq('21 Jun 2020')
+      expect(bottle1.drink_from).to eq('2018')
+      expect(bottle1.drink_to).to eq('2026')
+      expect(bottle1.bought).to eq('Dec 2017')
     end
 
     # two bottles
