ce202331d98cbd956de26c686da9b138f72f2805
Ryan Chambers <ryan.b.chambers@gmail.com>
2022-02-13 16:48:47 -0500

    Count grapes

--- db/queries.sql
+++ db/queries.sql
@@ -5,3 +5,14 @@
 --select * from wines where id = 283
 
 update wines set other = 'Antagonist', lcbo_code = '467662' where id = 283;
+
+
+-- count of grapes
+select g.id, g.name, coalesce(w.count, 0) as number_of_wines
+from grapes g
+left join
+(select grape_id, count(*) as count
+from wines
+group by grape_id)
+w on (g.id = w.grape_id)
+order by number_of_wines desc
